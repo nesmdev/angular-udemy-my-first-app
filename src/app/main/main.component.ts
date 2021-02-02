5b7efd1db20588f3a00ede27742fcdaa65b1e92d
ndev <ndev>
2021-02-02 11:12:14 -0500

    testing

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -2,11 +2,21 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-main',
-  template: ` <p>main works!</p> `,
+  template: `
+    <div class="container">
+      <h1>{{title}}!</h1>
+      <h2>{{person.name +" "+ person.lastname}}</h2>
+    </div>
+  `,
   styles: [],
 })
 export class MainComponent implements OnInit {
   constructor() {}
 
   ngOnInit(): void {}
+  title = "Main works!!";
+  person = {
+    name: "John",
+    lastname: "Doe"
+  }
 }
